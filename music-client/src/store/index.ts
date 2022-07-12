bdfeec9f611a85eb6eead134d6080166cdc03c3f
liyj2001 <74247293+liyj2001@users.noreply.github.com>
2022-07-12 19:55:43 +0800

    修改不会默认选择第一个menu菜单

--- music-client/src/store/index.ts
+++ music-client/src/store/index.ts
@@ -3,7 +3,7 @@ import {defineStore} from 'pinia'
 export const useStore = defineStore({
 	id: 'store',
 	state: () => ({
-		activeNav: '/index',
+		activeNav: '/',
 		keyword: null,
 		music: {
 			url: null,
